adc6adf5b67a601dc130088ff3f23b651db21b54
Maria Korosteleva <6837635+maria-korosteleva@users.noreply.github.com>
2023-01-17 23:23:38 +0100

    Revert "Sleeve top diff fix attempt"
    This reverts commit d4ce2a25a205f671995b658c792dbb54ab4cd067.

--- assets/GarmentCode/sleeves.py
+++ assets/GarmentCode/sleeves.py
@@ -4,17 +4,10 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 class SleevePanel(pyp.Panel):
-    def __init__(self, name, body, design, 
-        proj_shape, open_shape, opening_depth, end_width) -> None:
-        """Extra parameters are needed to calculate the shapes for front and back sleeves correctly
-            (they are slightly different, but overall calculation process is the same)
-        """
+    def __init__(self, name, body, design, opening_width, opening_depth, width_diff=0) -> None:
         super().__init__(name)
 
         # TODO Cuffs, ruffles start, fulles end, opening shape..
-
-        connecting_depth = design['connecting_depth']['v']
-        length = design['length']['v']
 
         pose_angle = np.deg2rad(body['arm_pose_angle'])
         shoulder_angle = np.deg2rad(body['shoulder_incl'])
@@ -22,25 +15,22 @@ class SleevePanel(pyp.Panel):
 
         base_angle = pose_angle if standing else shoulder_angle
 
-        # DEBUG
-        print('\n', self.name)
-        print(proj_shape.length(), proj_shape.shortcut_length())
-        print(open_shape.length(), open_shape.shortcut_length())
+        length = design['length']['v']
+        armhole = globals()[design['armhole_shape']['v']]
+        
+        proj_shape, open_shape = armhole(
+            opening_depth, opening_width, angle=base_angle, incl_coeff=0.2, w_coeff=0.1)
 
         open_shape.rotate(-base_angle)  
-        
-        # DRAFT end_width = (design['end_width']['v'] + width_diff) / 2 
+        arm_width = (design['opening_width']['v'] + width_diff) / 2   # DRAFT  abs(open_shape[0].start[1] - open_shape[-1].end[1])
 
         # Main body of a sleeve
         self.edges = pyp.esf.from_verts(
-            [0, 0], [0, -end_width], [length, -end_width]
+            [0, 0], [0, -arm_width], [length, -arm_width]
         )
         open_shape.snap_to(self.edges[-1].end)
         open_shape[0].start = self.edges[-1].end   # chain
         self.edges.append(open_shape)
-
-        print(self.edges)  # DEBUG
-        print(self.edges.lengths())
 
         # align the angle with the pose -- for draping
         self.edges.rotate(pose_angle) 
@@ -48,9 +38,7 @@ class SleevePanel(pyp.Panel):
         if standing:  # Add a "shelve" to create square shoulder appearance
             end = self.edges[-1].end
             len = design['standing_shoulder_len']['v']
-            self.edges.append(pyp.Edge(
-                end, 
-                [end[0] - len * np.cos(shoulder_angle), end[1] - len * np.sin(shoulder_angle)]))
+            self.edges.append(pyp.Edge(end, [end[0] - len * np.cos(shoulder_angle), end[1] - len * np.sin(shoulder_angle)]))
 
         # Fin
         self.edges.close_loop()
@@ -66,10 +54,7 @@ class SleevePanel(pyp.Panel):
 
         # Default placement
         self.set_pivot(open_shape[-1].end)
-        self.translate_to([
-            - body['sholder_w'] / 2 - opening_depth * 1.5, 
-            body['height'] - body['head_l'] + 7, 
-            0])  #  - low_depth / 2
+        self.translate_to([- body['sholder_w'] / 2 - opening_depth * 1.5, body['height'] - body['head_l'] + 7, 0])  #  - low_depth / 2
 
 
 class Sleeve(pyp.Component):
@@ -77,52 +62,19 @@ class Sleeve(pyp.Component):
     def __init__(self, tag, body, design, depth_diff) -> None:
         super().__init__(f'{self.__class__.__name__}_{tag}')
 
+        width = body['armscye_depth'] * 2
         design = design['sleeve']
         inclanation = design['inclanation']['v']
-
-        pose_angle = np.deg2rad(body['arm_pose_angle'])
-        shoulder_angle = np.deg2rad(body['shoulder_incl'])
-        standing = design['standing_shoulder']['v']
-
-        base_angle = pose_angle if standing else shoulder_angle
         
-        # Pre-compute opening shape
-        # TODO was it better when incl_smoothing was inside?
-        incl_smoothing = inclanation * design['smoothing_coeff']['v']   # smoothing distance for ArmholeSmooth
-        armhole = globals()[design['armhole_shape']['v']]
-        proj_shape_front, open_shape_front = armhole(
-            inclanation  + depth_diff, design['connecting_depth']['v'], angle=base_angle, 
-            incl_smoothing=incl_smoothing, w_coeff=design['smoothing_coeff']['v'])
-
-        proj_shape_back, open_shape_back = armhole(
-            inclanation, design['connecting_depth']['v'], angle=base_angle, 
-            incl_smoothing=incl_smoothing, w_coeff=design['smoothing_coeff']['v'])
-
-        print(
-            'Width!! ', 
-            self._arm_fabric_width(open_shape_front),
-            self._arm_fabric_width(open_shape_back))
-
-        w_front, w_back = self._arm_fabric_width(open_shape_front), self._arm_fabric_width(open_shape_back)
-        fr_frac = w_front / (w_front + w_back)
-
         # sleeves
         self.f_sleeve = SleevePanel(
             f'{tag}_f', body, design, 
-            proj_shape_front, open_shape_front,
-            inclanation + depth_diff, 
-            design['end_width']['v'] * fr_frac).translate_by([0, 0, 30])
+            width/2, inclanation + depth_diff, depth_diff).translate_by([0, 0, 30])
         self.b_sleeve = SleevePanel(
-            f'{tag}_b', body, design, 
-            proj_shape_back, open_shape_back,
-            inclanation, 
-            design['end_width']['v'] * (1 - fr_frac)).translate_by([0, 0, -25])
+            f'{tag}_b', body, design, width/2, inclanation, -depth_diff).translate_by([0, 0, -25])
 
         # DEBUG
-        print(
-            'Top Len Diff ', 
-            self.f_sleeve.interfaces['top'].edges.length(), 
-            self.b_sleeve.interfaces['top'].edges.length())
+        print('Top Len Diff ', self.f_sleeve.interfaces['top'].edges.length(), self.b_sleeve.interfaces['top'].edges.length())
 
         self.stitching_rules = pyp.Stitches(
             # DRAFT (self.f_sleeve.interfaces['shoulder'], self.b_sleeve.interfaces['shoulder']),
@@ -138,22 +90,10 @@ class Sleeve(pyp.Component):
             'out': pyp.Interface.from_multiple(self.f_sleeve.interfaces['out'], self.b_sleeve.interfaces['out'])
         }
 
-    def _arm_fabric_width(self, open_shape):
-        """Get the width of a sleeve fabic piece given 
-            the open shape of the armhole
-        """
-        s = open_shape.shortcut()
-        s = s[1] - s[0]
-
-        l = open_shape[0]  # edge perpendicular to the main fabic body
-        l = np.asarray(l.end) - np.asarray(l.start)
-        l = l / np.linalg.norm(l)
-
-        return np.dot(s, l)  
 
 
 # ------  Armhole shapes ------
-def ArmholeSquare(incl, width, angle, **kwargs):
+def ArmholeSquare(incl, width, angle=None, **kwargs):
     """Simple square armhole cut-out
         Not recommended to use for sleeves, stitching in 3D might be hard
 
@@ -172,16 +112,15 @@ def ArmholeSquare(incl, width, angle, **kwargs):
     return edges, sleeve_edges
 
 
-def ArmholeSmooth(incl, width, angle, incl_smoothing, w_coeff=0.2):
+def ArmholeSmooth(incl, width, angle=None, incl_coeff=0.2, w_coeff=0.2):
     """Piece-wise smooth armhole shape"""
-    
-    incl_diff = incl - incl_smoothing
-    edges = pyp.esf.from_verts([0, 0], [incl_diff, w_coeff * width],  [incl, width])
+    diff_incl = incl * (1 - incl_coeff)
+    edges = pyp.esf.from_verts([0, 0], [diff_incl, w_coeff * width],  [incl, width])
 
     sina, cosa = np.sin(angle), np.cos(angle)
     l = edges[0].length()
     sleeve_edges = pyp.esf.from_verts(
-        [incl_diff + l*sina, w_coeff * width - l*cosa], 
-        [incl_diff, w_coeff * width],  [incl, width])
+        [diff_incl + l*sina, w_coeff * width - l*cosa], 
+        [diff_incl, w_coeff * width],  [incl, width])
 
     return edges, sleeve_edges
