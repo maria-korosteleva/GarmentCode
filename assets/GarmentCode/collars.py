c2c7337061e0b4e13a77aa6bcaecb96a2898472b
Maria Korosteleva <6837635+maria-korosteleva@users.noreply.github.com>
2023-01-19 11:13:52 +0100

    new collar and organize parameters

--- assets/GarmentCode/collars.py
+++ assets/GarmentCode/collars.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 # Custom
 import pypattern as pyp
 
 
 # Collar shapes withough extra panels
-def VNeckHalf(depth, width):
+def VNeckHalf(depth, width, *args):
     """Simple VNeck design"""
 
     edges = pyp.EdgeSequence(pyp.Edge([0, 0], [width / 2,-depth]))
@@ -12,8 +14,16 @@ def VNeckHalf(depth, width):
 
 
 def SquareNeckHalf(depth, width):
-    """Simple VNeck design"""
+    """Square design"""
 
     edges = pyp.esf.from_verts([0, 0], [0, -depth], [width / 2, -depth])
+    
+    return edges
+
+def TrapezoidNeckHalf(depth, width, angle=90):
+    """Trapesoid neck design"""
+
+    angle = np.deg2rad(angle)
+    edges = pyp.esf.from_verts([0, 0], [-depth * np.tan(angle), -depth], [width / 2, -depth])
     
     return edges
