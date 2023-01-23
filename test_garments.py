42559ca05af4921826b5e49633c538f6ef50b17b
Maria Korosteleva <6837635+maria-korosteleva@users.noreply.github.com>
2023-01-23 17:53:18 +0100

    Sofia

--- test_garments.py
+++ test_garments.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     # body_file = './assets/body_measurments/f_smpl_model.yaml'
     # body_file = './assets/body_measurments/f_smpl_model_fluffy.yaml'
     # body_file = './assets/body_measurments/m_smpl_avg.yaml'
+    # body_file = './assets/body_measurments/sofia.yaml'
     with open(body_file, 'r') as f:
         body = yaml.safe_load(f)['body']
         body['waist_level'] = body['height'] - body['head_l'] - body['waist_line']
