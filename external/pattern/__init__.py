6cd98fb74bf7630363a9117be7c1540943b73b88
Maria Korosteleva <6837635+maria-korosteleva@users.noreply.github.com>
2023-04-12 15:20:07 +0200

    cairosvg transparency tests

--- external/pattern/__init__.py
+++ external/pattern/__init__.py
@@ -1,3 +1,8 @@
 """
     Package with various 2D garment pattern wrappers when pattern is given in custom .json format
 """
+
+
+# If Windows
+import os
+os.environ['path'] += r';C:\Program Files\Inkscape\bin'
