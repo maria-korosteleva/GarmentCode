3a7a955fe25255617e4c8055d1e87b92060de25c
Maria Korosteleva <6837635+maria-korosteleva@users.noreply.github.com>
2023-04-14 14:36:46 +0200

    smallfix

--- external/pattern/__init__.py
+++ external/pattern/__init__.py
@@ -1,8 +1,3 @@
 """
     Package with various 2D garment pattern wrappers when pattern is given in custom .json format
 """
-
-
-# If Windows
-import os
-os.environ['path'] += r';C:\Program Files\Inkscape\bin'
