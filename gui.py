ddb9fd01674502ed44da2ce35b303f977ad6780b
Maria Korosteleva <6837635+maria-korosteleva@users.noreply.github.com>
2023-04-14 14:37:47 +0200

    fix blurry appearance on windows

--- gui.py
+++ gui.py
@@ -1,5 +1,12 @@
+import os
 # Custom
 from gui.callbacks import GUIState
+
+if 'Windows' in os.environ.get('OS',''):
+    # https://stackoverflow.com/a/43046744
+    # Resolwing blurry fonts on Windows
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1)
 
 # TODO Instructions
 
