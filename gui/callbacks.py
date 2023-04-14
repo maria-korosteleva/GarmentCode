2da8cda785f5c1636efa6b10dff01e779dbecd25
Maria Korosteleva <6837635+maria-korosteleva@users.noreply.github.com>
2023-04-14 13:12:06 +0200

    gui refactoring

--- gui/callbacks.py
+++ gui/callbacks.py
@@ -1,17 +1,179 @@
 """Callback functions & State info for Sewing Pattern Configurator """
+
+# NOTE: PySimpleGUI reference: https://github.com/PySimpleGUI/PySimpleGUI/blob/master/docs/call%20reference.md
+
+# TODO allow changing window size? https://stackoverflow.com/questions/66379808/how-do-i-respond-to-window-resize-in-pysimplegui
+# https://stackoverflow.com/questions/63686020/pysimplegui-how-to-achieve-elements-frames-columns-to-align-to-the-right-and-r
+# TODO Visual appearance
+# https://github.com/PySimpleGUI/PySimpleGUI/issues/3412 for nice buttons & stuff
+# Native demo https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Simple_Material_Feel.py
+# TODO Icons
+
 import os.path
 from pathlib import Path
 from datetime import datetime
 import yaml
-import sys
 import shutil 
 import numpy as np
+import PySimpleGUI as sg
 
 
 # Custom 
 from assets.garment_programs.meta_garment import MetaGarment
 
-class State():
+class GUIState():
+    """State of GUI-related objects"""
+    def __init__(self) -> None:
+        self.window = None
+
+        # Pattern
+        self.pattern_state = GUIPattern()
+
+        # Pattern display
+        self.default_canvas_margins = [20, 20]
+        self.canvas_margins = self.default_canvas_margins
+        self.body_img_id = None
+        self.back_img_id = None
+
+        # Last option needed to finalize GUI initialization and allow modifications
+        self.window = sg.Window(
+            'Sewing Pattern Configurator', 
+            self.def_layout((842, 596)), 
+            finalize=True)
+        self.init_canvas_background()
+        self.upd_pattern_visual()
+
+
+    def __del__(self):
+        """Clenup"""
+        self.pattern_state.clear_tmp(root=True)
+        self.window.close()
+
+
+    def def_layout(self, canvas_size=(500, 500)):
+
+        # First the window layout in 2 columns
+
+        params_column = [
+            [
+                sg.Text('Body Mesurements: '),
+            ],
+            [
+                sg.In(
+                    default_text=self.pattern_state.body_file,
+                    size=(25, 1), 
+                    enable_events=True, 
+                    key='-BODY-'
+                ),
+                sg.FileBrowse(),
+            ],
+            [
+                sg.Text('Design parameters: '),
+            ],
+            [
+                sg.In(
+                    default_text=self.pattern_state.design_file,
+                    size=(25, 1), 
+                    enable_events=True, 
+                    key='-DESIGN-'
+                ),
+                sg.FileBrowse(),
+            ],
+        ]
+
+        # For now will only show the name of the file that was chosen
+        viewer_column = [
+            [sg.Graph(
+                canvas_size=canvas_size, 
+                graph_bottom_left=(0, canvas_size[1]), 
+                graph_top_right=(canvas_size[0], 0), 
+                background_color='white', 
+                key='-CANVAS-')
+            ],      
+            [
+                sg.Text('Output Folder:'),
+                sg.In(
+                    default_text=self.pattern_state.save_path, 
+                    expand_x=True, 
+                    enable_events=True, 
+                    key='-FOLDER-OUT-', ),
+                sg.FolderBrowse(size=6),
+                sg.Button('Save', size=6, key='-SAVE-')
+            ]   
+        ]
+
+        
+        # ----- Full layout -----
+        layout = [
+            [
+                sg.Column(params_column),
+                sg.Column(viewer_column),
+            ]
+        ]
+        return layout
+
+    def init_canvas_background(self):
+        '''Add base background images to output canvas'''
+        # https://stackoverflow.com/a/71816897
+
+        self.back_img_id = self.window['-CANVAS-'].draw_image(
+            filename='assets/img/background.png', location=(0, 0))
+        self.body_img_id = self.window['-CANVAS-'].draw_image(
+            filename='assets/img/body_sihl.png', location=self.canvas_margins)
+
+    def upd_pattern_visual(self):
+
+        print('New Pattern!!', self.pattern_state.png_path)  # DEBUG
+        if self.pattern_state.ui_id is not None:
+            self.window['-CANVAS-'].delete_figure(self.pattern_state.ui_id)
+
+        # Image body center with the body center of a body silhouette
+        # FIXME Still a little too low?
+        png_body = self.pattern_state.body_bottom
+        real_b_bottom = np.asarray([429/2 + self.canvas_margins[0], 530 + self.canvas_margins[1]])   # Not the very bottom  # TODO relative to resolution
+        location = real_b_bottom - png_body
+
+        # TODO Also if too far (e.g. after sleeve removal)
+        # TODO Change canvas size to fit a pattern? -> 
+        # TODO Bigger background image
+        # TODO Higher quality
+        if location[0] < 0: 
+            self.canvas_margins[0] -= location[0]
+            self.window['-CANVAS-'].delete_figure(self.back_img_id)
+            self.window['-CANVAS-'].delete_figure(self.body_img_id)
+            self.init_canvas_background()
+            location[0] = 0
+
+        self.pattern_state.ui_id = self.window['-CANVAS-'].draw_image(
+            filename=self.pattern_state.png_path, location=location.tolist())
+
+    def event_loop(self):
+        while True:
+            event, values = self.window.read()
+            if event == 'Exit' or event == sg.WIN_CLOSED:
+                break
+
+            # TODO Parameter update: change corresponding field
+            # TODO Any parameter updated: Update MetaGarment and re-load visualization
+
+            # TODO process errors for wrong files chosen
+            if event == '-BODY-':
+                file = values['-BODY-']
+                self.pattern_state.new_body_file(file)
+                self.upd_pattern_visual()
+            elif event == '-DESIGN-':  # A file was chosen from the listbox
+                file = values['-DESIGN-']
+                self.pattern_state.new_design_file(file)
+                self.upd_pattern_visual()
+            elif event == '-SAVE-':
+                self.pattern_state.save()
+            elif event == '-FOLDER-OUT-':
+                self.pattern_state.save_path = values['-FOLDER-OUT-']
+
+                print('PatternConfigurator::INFO::New output path: ', self.pattern_state.save_path)
+
+
+class GUIPattern():
     def __init__(self) -> None:
         self.save_path = os.path.abspath('./')   # TODO Use Path()
         self.png_path = None
