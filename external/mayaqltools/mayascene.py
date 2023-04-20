fix svg rotation error

external/mayaqltools/mayascene.py
@@ -22,6 +22,7 @@ import mtoa.core
 # My modules
 import pattern.core as core
 import pattern.wrappers as wrappers
+from pattern.utils import vector_angle
 from mayaqltools import qualothwrapper as qw
 from mayaqltools import utils
 reload(core)
@@ -750,7 +751,7 @@ class MayaGarment(wrappers.VisPattern):
         # Rotation
         curr_dir = sec_point - top_point
         target_dir = edge_3d[1] - edge_3d[0]
-        rot_angle = np.rad2deg(utils.vector_angle(curr_dir[:2], target_dir[:2])) 
+        rot_angle = np.rad2deg(vector_angle(curr_dir[:2], target_dir[:2])) 
         if not right:
             rot_angle = rot_angle - 360
 

external/pattern/wrappers.py
@@ -24,17 +24,7 @@ import matplotlib.pyplot as plt
 # my
 import customconfig
 from pattern import core
-
-def list_to_c(num):
-    """Convert 2D list or list of 2D lists into complex number/list of complex numbers"""
-    if isinstance(num[0], list) or isinstance(num[0], np.ndarray):
-        return [complex(n[0], n[1]) for n in num]
-    else: 
-        return complex(num[0], num[1])
-    
-def c_to_np(num):
-    """Convert complex number to a numpy array of 2 elements"""
-    return np.asarray([num.real, num.imag])
+from pattern.utils import *
 
 
 class VisPattern(core.ParametrizedPattern):
@@ -197,9 +187,16 @@ class VisPattern(core.ParametrizedPattern):
         # Z-fist rotation to only reflect rotation visible in XY plane
         # NOTE: Heuristic, might be bug-prone
         rotation = R.from_euler('XYZ', panel['rotation'], degrees=True)   # XYZ
-        rotation = rotation.as_euler('ZYX', degrees=True)
-        path = path.rotated(degs=-rotation[0], origin=list_to_c(vertices[0]))
 
+        # Estimate degree of rotation of Y axis
+        # NOTE: Ox sometimes gets flipped because of 
+        # Gimbal locks of this Euler angle representation
+        res = rotation.apply([0, 1, 0])
+        flat_rot_angle = np.rad2deg(vector_angle([0, 1], res[:2]))
+        path = path.rotated(
+            degs=-flat_rot_angle, 
+            origin=list_to_c(vertices[0])
+        )
         path = path.translated(list_to_c(translation))  # NOTE: rot/transl order is important!
 
         # TODO Collisions of non-2D panels when drawn together? 