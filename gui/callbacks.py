8438cb95d10f7e3e006ab4c54a4d0611c09a8dc4
Maria Korosteleva <6837635+maria-korosteleva@users.noreply.github.com>
2023-04-20 17:06:48 +0200

    gui special symbol confusion

--- gui/callbacks.py
+++ gui/callbacks.py
@@ -35,8 +35,8 @@ def Collapsible(layout, key, title='', arrows=(sg.SYMBOL_DOWN, sg.SYMBOL_RIGHT),
     # NOTE: from https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Column_Collapsible_Sections.py
 
     """
-    return sg.Column([[sg.T((arrows[1] if collapsed else arrows[0]), enable_events=True, k=key+'-BUTTON'),
-                       sg.T(title, enable_events=True, key=key+'-TITLE')],
+    return sg.Column([[sg.T((arrows[1] if collapsed else arrows[0]), enable_events=True, k=key+'#BUTTON'),
+                       sg.T(title, enable_events=True, key=key+'#TITLE')],
                       [sg.pin(sg.Column(layout, key=key, visible=not collapsed, metadata=arrows))]], pad=(0,0))
 
 icon_image_b64 = b'iVBORw0KGgoAAAANSUhEUgAAAQAAAAEBCAYAAACXLnvDAAAACXBIWXMAAAsSAAALEgHS3X78AAAU9ElEQVR4nO3dbXBU13kH8LuSVtIK7a7edhckIQQIkIN4sVkwMmAESDi8Gbt2HVzswZA2yYyT1h/cSafTzmTiyUw7/ZJMkpl0GmOndiYvTZrGTZqpTRkbJ0Mabz1gx2M3CY5jLGOBDHqX9vV2/rCqhbQC7Wr3nGfv+f9m+GKD7jn3PufRuec+91yXbdsWEZmphNedyFxMAEQGYwIgMhgTAJHBmACIDMYEQGQwJgAigzEBEBmMCYDIYEwARAZjAiAyGBMAkcGYAIgMxgRAZDAmACKDMQEQGYwJgMhgTABEBmMCIDIYEwCRwZgAiAzGBEBkMCYAgcLhcCv7RCqU8SzrEw6HayzLWl9Xu/hQaam7Ix6faB4du9zk84a+b1nWYSf11ecNfamzc+sDC6rqet3uyveSyfivL185/13Lss5EIpEBAU00Ej8Mokg4HO5qqGsN21ZqczKZCNu2XePx+EqXtmys9vlCVrBhuRVoWG797IW/j735m5O3RyKRMw7r//pbVu787z09ny+/1H/Outh/zhoa6rN+/+4rI+PjQ0mXyzVQXl71eiqZeKn/8juRSCTyooBmOx4TQJ4h0H3e4GpPpb8nkYxuGBsbbPV6A4nmxjU1Pm/o6iAPNiyzKiqqMx74G8c/MXbq5RMLivgUzOrObd2jnzn2vapM/z8aHbEu9r9tITkMDfdZ773/+sDw8KWyqir/O2WlFf8zPjH4wtDwxTeclhh14y1Ajian76HAikdSdrIN03eXy1W/bMmmilBwpbu5aZ3l94Uw9Z3zARD4Ho/vbWFdzRv0bWi4ryPTOUFCXNy09uqfNJxfnJOOwaG+jvd6zx7pu/ibeE/3waht2x/iNqLEVfq7vku/fZq3EbljApiDyel7SWnZ9lhsbA2m7wtDq2ZM3+frfO9Zy7btX8g+G7lD3873nu1Y3b57zj8DyQJ/0onBnf5Tfan/3JKL/ee2DA31HcFtxK6dB3gbkQMmgBvo6T74qtvtWXrbunvnNH2fL9wTpxfGHAl9Gxrq+3Q++haYknQ7Nz189YJEoyP+i/1vL7nUf24/ZlN79xwaiMfHf//CiR/f5tRzOl9MADeQTMaXHD18vEbV8fCbDNNZVcfT4Az6ODlg8y3TbcQ3//nhJQ4+n/PGOoAbqKz0KT0/WAhz8r0s+oY+qqT6GhYbnpxZ4L4fK/cqj4nFLZXH00F1H3ENcS3FnABhmABmEQy07Qs0LBPZNpo7XENcS56yzJgAZtfu8y5UdrDzva9ZWMFWdkBN0Ef0VRVcw0QiusHp5zVXTACzwOO+KYtJSrgsl+NvAVT3EdcwlUpyKjcLJoBZoKhHZMMoa7yWs2MCyADlvH7fIp4bh6j1N1Xgmpp+HjJhkGeAWv5FofaMNeuF8l7vWQv17hq7rQT6iL6qhNJsXFOnn9tcMAFkgBd5UMuv2tDwxV5ZZyL/dPQR1xLXVPVxiwETQAbR2GgXXuQhZ0D5Nq4pL+dMTAAZpFIJbzZv8ZFsKBHGNeVlmokJYBq85ltVVVsuqlE0b7im6Ve4aQomgJnW4zVf1QfFbym8cqz6uKqhjz4Nt1fpa8onAdMwAUyDslEdAYpXW7EvoPIDK4Y+qqywnHR13waWBM/ABDCNbac6g3nY3INkwTXFteVluR4TwDTY2isfu/uQLLimuLa8LNdjApiGZaPOxWs7ExPAFCgXXRhcpeUJgCnPqtHHoKbXrHFtWRJ8PSaAKVAuWuNv1JIACrXPoES6+opry5Lg6zEBTKGrBHhSMhl35PcAptLZR5YEz8QEMIXO6al1dd98v+M3adXZR5YEz8QEMEU0Olpv0lTcNLi2uMamn4epmADSUCbq96svUJnK6RtY6thodTpcY5YEf4QJ4CNaSoCnqih3/BKA9j6yJPh6TABp+MafjhLgqVCsgnZobUQBoW86F1mtdEmwk89xtpgA0vCBT90lwNiDAO3Q2ogCQt8qK/TOAHCNnXyOs8UEkDYxMbxKdwmw08tVJZRZ4/i41lobIQgTQJqUR3D48rCAZhSElL6Z8Lh1rpgA0iXA9bUtSjcBnU1tTZM7HA63SmhLPqFP6JuEtuBasyT4GiaAa5tUdIeCK0XsApTejdhxCQB9Ur3T8mxwrXHNJbRFNyaA9CYVUl4BduqTAPRJ0jk2YfOVuWACEFACPFUwsNxKJJ33LTv0CX2TgCXBH2ECSC9OSSkBxm7EY2ODjrsFGB8fWiZlp2VcaycvtmbD+ASAslCPx1cqoCn/LxRcUeKkhUD0JRiQ9egd15wlwUwAloQS4OmwWNZQ13q/pDbNB/oiZQFwEkuCrzE+AUhanJqEctmS0rLtMlozf+iL7hLg6Zxedj1XxieAWHxsrbTPgOGb9uPjg5sFNCUv0Bf0SRJcc1z7Yj+382V8AkgkYosl7gK8YEFdpRPWAdAH9EVAU66Da45rL6hJWhifAKSWheIe1QnrAOhD+4odIndZYUmw4QkAG1Q01C8VuRLctuwOy1VSco+ApswL+iBt+j8J197JG7DMhdEJAN+pk/oVIExRR0Y+lLVylgP0QeqHVnDtTfge440YnQCwOi35K0CtLRuKeh97tB19ENCUjHDtnfS0JRdGJ4BYbGyN1OkprFi+rby+ruVvBDQlJ2g7+iC1fbj2iAEBTdHGyASACrDdPfee9Hj8sp7/TXMtQMeL9jfU6NjAbskJ1rq2EBhCLJhaFWhcAujedeCLC6pqL2y87YEdD973ZXGPp6ZCzXp93ZKqYnwciDYvDK4slb7NOmIAsYCYQGwIaJJSxjwG2b/v8LaRkf5/Wdl2Z6hz40NF8ymuVSu2V42NDzxqWdZfCmjOnAUDbY+i7cXQ1tXtu622pXdUnn7l2b/t2n7Xp6qrG/74Jz/99ssCmlZwLtu2Hd1BTO2CgbZnSkvd3d3bP1dZbJ/+jkZHrCefPTr885+f9Alozpxt3bpz6JMPPeUttg+tXOo/Z5146asTyWT8xMVLv3s4EokMCGhWwTj6FuDju+9/vMpT03vr2oP7MdUrxu/+YwA1htori+lpANqKNhfjV5YQI4gVxAxiBzEkoFkF48hbAARgjb/x3xaF2hftvPPR8mL/3FfHx/a4R8YuP2FZ1gEBzbmpYKDtCbRZeDNvKH1bUHXy1Ne/1L3r7s8ODL5/TyQSOSO4yTlx1C0Apvv1dS3fxEDZeefnyqWvQGfjG8c/MXbq5RNF8emgO7d1j37m2PeK4v5/Ls73vmadPPXVmGVZ//7h5Xf/1Em3BY65BTiw76FPYcq25mN77zvy4D85avBD+8odVcUwHUUb0VYBTckbxBJiCrGFGEOsOaRrxT8DwOOmGn/ji06Z7s9maLjP+sGP/+oPJ/7rOdGPBLt33f3O/Qf/bomU7b/yDYuyJ099PXah760LA4Pvd0UikXeKuT9FmwAw3Q8FVnw5Fh9/oLvrLzxO+42fyXd++NjEhQ/e7JR6L4q1l0ULbzktvb4iH3BbcOLFr4yXuz3f77v028eK9bagKG8Bdu7Ye9jnDZ5b1nr7kaOHnzRi8MPGWx+oDDQs+5qApmSEtqGNApuWd4g5xB5iELGImCzGfhTVDADTfZ83dKKhbknLzu2fdTt1mnkj//jUoYnRsSu3SJt6Xt34o6r2zU8f/a4RCWAq3J6dfOlr8f7Lf3h3aLivu5huC4pmBrBvz4NPVy+o/9+7dj2+/J79Txg5+GFr57HKUGDFFwQ05TpoE9omqEnKIBYRk4hNxChitVjaLn4GsLvn3v2JRPRbq9t313VuelhAi/R76tvHRq4M9C6Wct+J9ZjamqbzRw8fd+YKbJZO/+oZ6423nr9cVlZx5PkXfvQTyW0VmwDSH5P811p/U4ep0/3ZvPHW89aZ15771k9/9h0Ru9riN976tXcfQfEMXTN5W3BlsPfXVwZ6/0jqbYHYWwBMKdd1HLjV5On+bDDQYvGx+yS8JYg2oC0c/NebvC1ADEu8ZZskNgGk7GSbKav7udi04VB1Xe3i47rbgTagLbrbIRViGLEstX1iE0A8PtFcjC/vqILfuC6Xa4vOl4Rw7IqK6k7+9p8dYhixLLV9xm8LXszwvgNeetLVBRx762YzV/6dQmwCcLlc9QKaIRqmlyiB1vGOAI6JY/M27eYkx7LYBOB2exICmiEe3n8Ynxh8QuWedjgWjoljF++ZU0dyLItMAFhZrvLUMLjmAC8/7dr+55X1dS3KtrDCsXBMp754lW+IZan7OkqdAbRK+5y0ZPiKULChbeXePYf+odDNxDFwLByT5iYdy0wAWTByi+b5wHQ8Hp94FJufFuoY+Nk4Bqf+OREZ0yITQCiw4h4+AswOpuP77/prz+DgB/9RiPUA/Ez8bByDU//sIJYR0xLbJnYRkEGWPQRaz47HqqsX1L+VzySAn4WfiZ/NxJw9ybEsMgEkktENfh/Lf3OBe/Mtmx8Jeasbfpmvn4mfhZ/J+/7cIJYR0xLbJnJX4GQy4WX9f+7SlXmreroPvnploHdnrm8Npt/yO3nH7UdWsdovd4hlxLTEtomcASST8aLY/VYyDNhNGw7dWlVV+0YutwP4N/i3+Bkc/PMnNaZFJgCPx2/MJ8sKCQN3W+exRmxZlc3TAfxd/Bv8Ww7+/JAa03wXwOEwgA/u/UJdNDr6n3OpE8Dfwd/Fv+Hgdz5xCQBvmNXXtrAIKI+wcv8n93/Fs7x18+Nd2+/6ALssTf/p+G/4f/g7+Ltc7c8vxLTEz7tJnJbU1PgbWWiSZ3gUhS3VVt+yO/SLXz79w+5dd18oKSl70rZTlm2nPokXe7ZsfqSci6+FkY5pccVA4hKAzxtsEtAMx8IA39Pz+fKh4b4lr5790ReHR/qt7Vv+zOLALzyJsS3uFsBT6e9pblonoCXOhgG/fOkdVtOi1Rz8CiCmEdvS2sVFQCKDiUsArAIkJ5JaDSguAbAKkJxIajWgwATAKkByJomxLW8RkFWA5FASY5uLgEQGE5UAWAVITiaxGlDaDIBVgORYEqsBRSWAhrrWsIBmEBWMtBgXlQBKS90drAIkp0JsI8YldY+LgEQGE5UAorHRrmDDMgEtIco/xDZiXNKpFTcD4G7A5FQSY1tUAkilZG6cSJQv0mJcVAKorPRxTYIcTVqMc8ARGUxMAgiHw13NjWv4TUByNMQ4Yl1KH0XNACrK+SIgOZu0GBeTAFAhxScA5HSIcUnVgGISACqkuBU1OR1iXFI1IBcBiQwmJgGwCpBMIK0aUNYiINcAyOGkxbiYBMAqQDKFpFgXkwBYBUimkBTrHHREBhORAFgFSCaRVA0oZgbAKkAyhaRYF5EAgoG2fT5+DowMgVhHzEvorYgE4LJcAZ93oYCWEBUeYh0xL+FUi0gAtmXXC2gGkTJSYl5EAojFxtYsbloroCVEhYdYR8xLONV8DEhkMBm3ALbNR4BkFCkxLyIBVFQssAU0g0gZKTGvPQGEw+GakpJSfhKcjIKYR+zr7rOEGcD6pS0b+RogGSUd89q/FMxFQCKDaU8ArAIkE0mpBtSeAFgFSCaSUg2oPQGwCpBMJSH2tScAVgGSiaRUA3IRkMhg+m8BWAVIhpIQ+9oTAKsAyVQSYl9rAgiHw62lpe5KnW0g0gWxjzGg8wLongG0ti7ewARARkrHvtEJgIg00poAQoEVj/CDoGQqxD7GgM7uC1gE5HtAZCYJsa81AaTsZFtlBbcDJzMh9jEGdHZeawKIxyeaeQtApkLsYwzo7D4XAYkMpjUBuFwuvghERtM9BrQmALfbk9B5fCLddI8BbQkAFVBVnppyXccnkgBjQGc1oM4ZQOuiUHuVxuMTaZceA0YmACLSTFsCYBUgkf5qQK0zAFYBkul0jwFtCYBVgET6qwG1JQBWARLprwbkIiCRwbQlAFYBEl2jcyxoSwCsAiS6RudY0JIAwuHw+vraFhYBEVmWhbGAMaHjXOiaAdTU+BtZBkyEwXBtLGjZIlxLAvB5g006jkskla4xoSUBeCr9Pc1N63QcmkgcjAWMCR3t4mNAIoNpSQCJZHSD3xdi3BFZloWxgDGh41xoSQDJZMLr8zIBEFlX7/9DV8eEjpOhKQHE+RIA0RS6xoSeRUCPv0zHcYmk0jUmuAhIZDDlCYBVgEQz6aoG1DEDYBUg0TS6qgGVJwBWARJlpmNsKE8ArAIkmklXNSAXAYkMpjwBsAqQaCZd1YDKEwCrAIlm0lUNqDwBRKOj3AqMKAMdY0N5AvB6A9wKjCgDHWODi4BEBlOaAMLhcFdz4xotWx8RSYexgTGispnKZwAV5XwRkCgTHWNDaQJoqGsN83uARJlhbGCMqDw9ShNAaam7g58DI8oMYwNjROXp4SIgkcGUJoBobLQr2LCM8UaUAcYGxojKc6N+EZBrAEQZ6RgbShNAKqVn40OiYqF6jChNAJWVPq45EN2A6jHCAUlkMGUJgFWARDenuhpQ6QyAVYBEN6Z6jChLAKwCJLo51dWAyhIAqwCJbk51NSAXAYkMpiwBsAqQ6OZUVwOqXQTkGgDRDakeI8oSgG3bfARINAcqx4qyBFBRscBWdSyiYqZyrChJAOFwuKakpJSfBCeaA4wVjBkV50rVDGD90paNXAAgmoP0WFHypWA+BiQymJIEEAy07fPxc2BEc4KxgjGj4mwpSQAuyxXweReqOBRR0cNYwZhR0Q8lCcC2bH4OjCgLqsaMkgQQi42tWdy0VsWhiIoexgrGjIp+cBGQyGBqbgFYBUiUFVVjRkkCYBUgUXZUjZmCJwBWARJlT1U1oIoZAKsAibKkqhqQi4BEBit4AmAVIFH2VFUDFjwBsAqQKHuqqgELngBSdrKt0McgciIVY6fgCSAen2hmFSBRdjBmMHYKfdq4CEhksMKvAbhcfBGIKAcqxk7BC3RisbGy0796ptCHycnA4PuxD6+8OyaycZZlJRLR0VQqmSjgIcqTybh15vXnYoU6AApabDvld7s9hexHzuprW6pq/I3lEtuGsVPoYxT8AKNjV/acfuXZQh8mVwORSOSM1MZR4YXDYRTbGPuuisu2WaZPZCouAhIZjAmAyGBMAEQGYwIgMhgTAJHBmACIDMYEQGQwJgAigzEBEBmMCYDIYEwARAZjAiAyGBMAkcGYAIgMxgRAZDAmACKDMQEQGYwJgMhgTABEBmMCIDIYEwCRwZgAiExlWdb/AZUAlsPEgP73AAAAAElFTkSuQmCC'
@@ -153,7 +153,13 @@ class GUIPattern():
 
 
 class GUIState():
-    """State of GUI-related objects"""
+    """State of GUI-related objects
+    
+        NOTE: "#" is used as a separator in GUI keys to avoid confusion with
+            symbols that can be (typically) used in body/design parameter names 
+            ('_', '-', etc.) 
+
+    """
     def __init__(self) -> None:
         self.window = None
 
@@ -178,8 +184,8 @@ class GUIState():
             finalize=True)
         
         # Modifiers after window finalization
-        self.input_text_on_enter('BODY-')
-        self.input_text_on_enter('DESIGN-')
+        self.input_text_on_enter('BODY#')
+        self.input_text_on_enter('DESIGN#')
         self.prettify_sliders()
         self.init_canvas_background()
         self.init_body_silhouette()
@@ -339,6 +345,8 @@ class GUIState():
 
         fields = []
         for param in design_params:
+            if '#' in param:
+                raise ValueError(f'GUI::ERROR::parameter name {param} contains special symbol #. Please, rename the paramer to avoing this symbol')
             if 'v' in design_params[param]:
 
                 p_type = design_params[param]['type']
@@ -352,13 +360,13 @@ class GUIState():
                         values=design_params[param]['range'], 
                         default_value=design_params[param]['v'],
                         enable_events=True,
-                        key=f'{pre_key}-{param}',
+                        key=f'{pre_key}#{param}',
                     )
                 elif p_type == 'bool':
                     in_field = sg.Checkbox(
                         param, 
                         default=design_params[param]['v'], 
-                        key=f'{pre_key}-{param}',
+                        key=f'{pre_key}#{param}',
                         enable_events=True, 
                         expand_x=True)
                 elif p_type == 'int' or p_type == 'float':
@@ -368,7 +376,7 @@ class GUIState():
                         orientation='horizontal',
                         relief=sg.RELIEF_FLAT, 
                         resolution=1 if p_type == 'int' else 0.05, 
-                        key=f'{pre_key}-{param}', 
+                        key=f'{pre_key}#{param}', 
                         enable_events=True
                     )
                 else:
@@ -376,7 +384,7 @@ class GUIState():
                     in_field = sg.Input(
                         str(design_params[param]['v']), 
                         enable_events=False,  # Events enabled outside: only on Enter 
-                        key=f'{pre_key}-{param}', 
+                        key=f'{pre_key}#{param}', 
                         size=7) 
 
                 fields.append(
@@ -397,10 +405,10 @@ class GUIState():
                             Collapsible(
                                 self.def_flat_design_layout(
                                     design_params[param], 
-                                    pre_key=f'{pre_key}-{param}'
+                                    pre_key=f'{pre_key}#{param}'
                                 ), 
                                 title=param,
-                                key=f'COLLAPSE-{pre_key}-{param}'
+                                key=f'COLLAPSE-{pre_key}#{param}'
                             )
                         ])
                 else:
@@ -410,9 +418,9 @@ class GUIState():
                                 param,
                                 self.def_flat_design_layout(
                                     design_params[param], 
-                                    pre_key=f'{pre_key}-{param}'
+                                    pre_key=f'{pre_key}#{param}'
                                 ), 
-                                key=f'COLLAPSE-{pre_key}-{param}',
+                                key=f'COLLAPSE-{pre_key}#{param}',
                                 expand_x=True
                             )
                         ])
@@ -425,6 +433,9 @@ class GUIState():
         design_params = self.pattern_state.design_params
         tabs = []
         for param in design_params:
+            if '#' in param:
+                raise ValueError(f'GUI::ERROR::parameter name {param} contains special symbol #. Please, rename the paramer to avoing this symbol')
+  
             if 'v' in design_params[param]:
                 sg.popup_error_with_traceback(
                     f'Leaf parameter on top level of design hierarchy: {param}!!'
@@ -436,7 +447,7 @@ class GUIState():
                     param, 
                     self.def_flat_design_layout(
                         design_params[param], 
-                        pre_key=f'{pre_key}-{param}', 
+                        pre_key=f'{pre_key}#{param}', 
                         use_collapsible=(param == 'left')
                     ))
             )
@@ -447,7 +458,6 @@ class GUIState():
                     tab_border_width=0, 
                     border_width=0
                 )]]
-
 
     def init_canvas_background(self):
         '''Add base background image to output canvas'''
@@ -534,9 +544,9 @@ class GUIState():
         """Update current values of the fields 
             if they were loaded from a file
         """
-        fields = self.get_keys_by_instance_tag(sg.Input, 'BODY-')
+        fields = self.get_keys_by_instance_tag(sg.Input, 'BODY#')
         for elem in fields:
-            param = elem.split('-')[1]
+            param = elem.split('#')[1]
             self.window[elem].update(self.pattern_state.body_params[param])
     
     def upd_fields_design(self, design_params, pre_key='DESIGN'):
@@ -545,11 +555,11 @@ class GUIState():
         """
         for param in design_params:
             if 'v' in design_params[param]:
-                self.window[f'{pre_key}-{param}'].update(design_params[param]['v'])
+                self.window[f'{pre_key}#{param}'].update(design_params[param]['v'])
             else:
                 self.upd_fields_design(
                     design_params[param], 
-                    f'{pre_key}-{param}'
+                    f'{pre_key}#{param}'
                 )
 
     # Modifiers after window finalization
@@ -560,7 +570,7 @@ class GUIState():
         # All body updates
         fields = self.get_keys_by_instance_tag(sg.Input, tag)
         for key in fields:
-            self.window[key].bind('<Return>', '-ENTER')
+            self.window[key].bind('<Return>', '#ENTER')
 
     def prettify_sliders(self):
         """ Make slider knowbs flat and small
@@ -596,11 +606,11 @@ class GUIState():
             if event == 'Exit' or event == sg.WIN_CLOSED:
                 break
             elif event.startswith('COLLAPSE'):
-                field = event.removesuffix('-BUTTON').removesuffix('-TITLE')
+                field = event.removesuffix('#BUTTON').removesuffix('#TITLE')
                 # Visibility
                 self.window[field].update(visible=not self.window[field].visible)
                 # UPD graphic
-                self.window[field + '-BUTTON'].update(
+                self.window[field + '#BUTTON'].update(
                     self.window[field].metadata[0] if self.window[field].visible else self.window[field].metadata[1])
             elif event == 'BODYDISPLAY':
                 # Toggle display of body figure
@@ -623,10 +633,10 @@ class GUIState():
                     self.upd_fields_body()
                     self.upd_pattern_visual()
 
-                elif event.startswith('BODY-') and '-ENTER' in event:
+                elif event.startswith('BODY#') and '#ENTER' in event:
                     # Updated body parameter:
-                    param = event.split('-')[1]
-                    new_value = values[event.removesuffix('-ENTER')]
+                    param = event.split('#')[1]
+                    new_value = values[event.removesuffix('#ENTER')]
 
                     try:   # https://stackoverflow.com/a/67432444
                         self.pattern_state.body_params[param] = float(new_value)
@@ -639,10 +649,10 @@ class GUIState():
                         self.pattern_state.reload_garment()
                         self.upd_pattern_visual()
 
-                elif event.startswith('DESIGN-'): 
+                elif event.startswith('DESIGN#'): 
                     # Updated body parameter:
-                    event_name = event.removesuffix('-ENTER')
-                    param_ids = event_name.split('-')[1:]
+                    event_name = event.removesuffix('#ENTER')
+                    param_ids = event_name.split('#')[1:]
                     new_value = values[event_name]
 
                     nested_set(
