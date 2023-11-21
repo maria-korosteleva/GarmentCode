88f04fc98b483a80e325b5e880414ff647ec817c
Maria Korosteleva <6837635+maria-korosteleva@users.noreply.github.com>
2023-11-21 15:15:43 +0100

    hood edits

--- assets/garment_programs/collars.py
+++ assets/garment_programs/collars.py
@@ -253,36 +253,44 @@ class SimpleLapel(pyp.Component):
 
 class HoodPanel(pyp.Panel):
     """A panel for the side of the hood"""
-    def __init__(self, name, f_depth, b_depth, width, length) -> None:
+    def __init__(self, name, f_depth, b_depth, width, in_length, depth) -> None:
         super().__init__(name)
 
         width = width / 2  # Panel covers one half only
+        length = in_length + width / 2    # DRAFT np.sqrt(in_length**2 + width**2)  # Account for "flattening" in the neck area
+
+        # DEBUG
+        print(in_length, length, width, in_length**2 + width**2)
 
         self.edges = pyp.EdgeSeqFactory.from_verts(
-            [-width, -b_depth], [0, 0], [width, -f_depth], [width, length],
+            [-width, -b_depth], [0, 0], [width, -f_depth], [width * 1.2, length], [width * 1.2 - depth, length]
         )
         self.edges.append(
             pyp.CurveEdge(
                 self.edges[-1].end, 
                 self.edges[0].start, 
-                [[0.3, -0.6]]
+                [[0.2, -0.5]]
             )
         )
 
         self.interfaces = {
-            'to_other_side': pyp.Interface(self, self.edges[-1]),
+            'to_other_side': pyp.Interface(self, self.edges[-2:]),
             'to_bodice': pyp.Interface(self, self.edges[0:2]).reverse()
         }
 
         self.rotate_by(R.from_euler('XYZ', [0, -90, 0], degrees=True))
-        self.translate_by([0, 0, -width * 2])
+        self.translate_by([-width * 1.5, 0, 0])
 
 class Hood2Panels(pyp.Component):
 
     def __init__(self, tag, body, design) -> None:
         super().__init__(f'Hood_{tag}')
 
-        depth = design['collar']['component']['depth']['v']
+        depth = design['collar']['component']['depth']['v'] 
+
+        # TODO circle back
+        # TODO arbitraty front? 
+        # TODO design parameters
 
         # --Projecting shapes--
         # DRAFT 
@@ -299,12 +307,13 @@ class Hood2Panels(pyp.Component):
         #     design['collar']['bc_depth']['v'],
         #     design['collar']['width']['v'])
 
+        width = design['collar']['width']['v']
         f_collar = VNeckHalf(
             design['collar']['fc_depth']['v'],
-            design['collar']['width']['v'])
+            width)
         b_collar = VNeckHalf(
             design['collar']['bc_depth']['v'],
-            design['collar']['width']['v'])
+            width)
         
         self.interfaces = {
             'front_proj': pyp.Interface(self, f_collar),
@@ -313,16 +322,16 @@ class Hood2Panels(pyp.Component):
 
         # -- Panel --
         length_f, length_b = f_collar.length(), b_collar.length()
-        height_p = body['height'] - body['head_l'] + depth * 2
         
         self.panel = HoodPanel(
             f'{tag}_hood', 
             design['collar']['fc_depth']['v'],
             design['collar']['bc_depth']['v'],
-            design['collar']['width']['v'],
-            body['head_l'] * 1.4
+            width,
+            body['head_l'], # DRAFT + body['neck_w'] / 2,  # DRAFT  * 1.4,  # TODOLOW It would be better to use head width measurement
+            depth=width / 2
         ).translate_by(
-            [-depth * 2, height_p, 30])
+            [0, body['height'] - body['head_l'] + 10, 0])
 
         self.interfaces.update({
             #'front': NOTE: no front interface here
