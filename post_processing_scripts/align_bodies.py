94f52b6aac0763ac8dd1ccac40f8ce50ab2efea3
Maria Korosteleva <6837635+maria-korosteleva@users.noreply.github.com>
2024-07-24 21:55:30 +0200

    extra scripts from old repo

---
+++ post_processing_scripts/align_bodies.py
@@ -0,0 +1,55 @@
+"""Aling body models s.t. they stand exactly on the plane y=0 and save as a new data"""
+import igl
+import numpy as np
+from pathlib import Path
+
+from meshgen.external.customconfig import Properties
+
+def load_mesh(path):
+    v, f = igl.read_triangle_mesh(str(path))
+    return v, f.flatten(), f
+
+def get_shift_param(body_vertices):
+    v_body_arr = np.array(body_vertices)
+    min_y = (min(v_body_arr[:, 1]))
+    if min_y < 0:
+        return abs(min_y)
+    return 0.0
+
+def save_mesh(path, v, f):
+    igl.write_triangle_mesh(str(path), v=v, f=f, force_ascii=False)
+
+
+def process_body(path_in, path_out):
+
+    body_vertices, _, body_faces = load_mesh(path_in)   #  self.paths.in_body_obj)
+    shift_y = get_shift_param(body_vertices)  
+
+    # body_vertices = body_vertices * b_scale
+    if shift_y:
+        body_vertices[:, 1] = body_vertices[:, 1] + shift_y
+    
+    save_mesh(path_out, body_vertices, body_faces)
+
+if __name__ == "__main__":
+
+    system_paths = Properties('./system.json')
+    
+    body_folder_path = Path(system_paths['body_samples_path']) / 'body_shapes_and_measures_2023-12-30'
+    body_objs_path = body_folder_path / 'meshes'
+    out_path = body_folder_path / 'meshes_aligned'
+    out_path.mkdir(parents=True, exist_ok=True)
+
+    # body_path = r"C:\Users\mariako\Documents\Code\garmentsimwarp\assets\default_bodies\mean_all_apart.obj"
+    # out_body_path = r"C:\Users\mariako\Documents\Code\garmentsimwarp\assets\default_bodies\mean_all_apart_y_aligned.obj"
+    # process_body(body_path, out_body_path)
+
+    # loop over all meshes
+    for file in body_objs_path.iterdir():
+        process_body(file, out_path / file.name)
+
+        # DEBUG
+        print(file.name)
+
+    
+
---
+++ post_processing_scripts/gather_renders.py
@@ -0,0 +1,25 @@
+"""In simulated dataset, gather all the scene images in one folder"""
+
+import meshgen.external.customconfig as customconfig
+from pathlib import Path
+import shutil
+
+from datasim import gather_renders
+
+
+system_props = customconfig.Properties('./system.json')
+dataset = 'unpacking_test'
+datapaths = [
+    Path(system_props['output']) / dataset / 'default_body', 
+    Path(system_props['output']) / dataset / 'random_body'
+]
+
+for datapath in datapaths:
+    # Check packing
+    tar_path = datapath / 'output.tar.gz'
+    if tar_path.exists():
+        shutil.unpack_archive(tar_path, datapath)
+        # Finally -- clean up
+        tar_path.unlink()
+
+    gather_renders(datapath)
